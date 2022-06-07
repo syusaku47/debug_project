9d96ec4a7d3ee5f9fb3b22892e9d9b489ba602bc
syusaku47 <carryppp@gmail.com>
2022-06-08 00:36:20 +0900

    [Add] デバッグ経過時間表示

--- app/Http/Controllers/debugController.php
+++ app/Http/Controllers/debugController.php
@@ -6,8 +6,31 @@ use Illuminate\Http\Request;
 
 class debugController extends Controller
 {
-    public function test()
+    public function index()
     {
-        dd('test');
+        $start = date("YmdHis") . '.' . substr(explode(".", microtime(true))[1], 0, 4);
+        \Log::info("start time:" . $start);
+
+        // 処理を記載
+        foreach (range(1, 1000000) as $value) {
+            if($value === 1000000){
+                break;
+            }
+        }
+        foreach (range(1, 1000000) as $value) {
+            if($value === 1000000){
+                break;
+            }
+        }
+
+
+        $end = date("YmdHis") . '.' . substr(explode(".", microtime(true))[1], 0, 4);
+        \Log::info("end time:" . $end);
+
+        return view('debug.index', [
+            'start' => $start,
+            'end' => $end
+        ]);
+
     }
 }
